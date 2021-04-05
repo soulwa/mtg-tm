cb7cf3d616859ac287406a321c38fe6beb8708cd
Sam Lyon <sam.c.lyon@gmail.com>
2021-04-05 14:53:44 -0400

    cleanup, working utm-exec modeled after the tmi function

--- utm.lisp
+++ utm.lisp
@@ -4,26 +4,18 @@
   '(a b c d e f g h i j k l m n o p q r))
   
 (defconst *even-huh-tag-sys*
-  '(2 ( (a ()) (b (b)) (c (b c))) (c c a a a a)))
-  
-
-
+  '(2 (a ()) (b (b)) (c (b c)) (c c a a a a)))
 
 (defun utm-symp (x)
   (member x *alphabet*))
 
-;;listof symbol
-;;not programmatically different from utm-tape- but this one
-;;is used for production functions
+;; listof symbol
+;; not programmatically different from utm-tape- but this one
+;; is used for production functions
 (defun utm-losp (x)
   (cond ((endp x) T)
         ((consp x) (and (utm-symp (car x))
                         (utm-losp (cdr x))))))
-#|
-(defun append (x y)
-  (cond ((endp x) y)
-        ((consp x) (cons (car x) (append (cdr x) y)))))
-|#
 
 (defun remove-n (n x)
   (if (zp n)
@@ -41,7 +33,7 @@
 
 ;; production function
 ;; a cons of a symp to a los-symp
-
+;; 
 (defun utm-funcp (x)
   (and (consp x)
        (= (length x) 2)
@@ -68,17 +60,15 @@
 ;; return the applicable func from a set
 ;; assumes such a func exists
 (defun utm-funcs-applicable (x y)
-  (cond ((endp x) 'ERROR)
+  (cond ((endp x) nil)
         ((consp x) (if (utm-func-appliesp (car x) y)
                        (car x)
                        (utm-funcs-applicable (cdr x) y)))))
 
-
-
-
 ;; a tag system has a tape,
 ;; deletion number, and prod. function
 ;; Alphabet is included in the defintion of tape
+;; '(,deletion-number ,prod-funcs ,tape)
 (defun tag-sysp (x)
   (and (consp x)
        (equal (length x) 3)
@@ -91,14 +81,22 @@
 ;; using the first element of the tape and the set of production functions
 ;; append the application of that function to the end
 ;; delete the deletion number from the head
-(defun utm-step (x)
-  (list (first x) (second x) (remove-n (first x) (append (third x) (utm-func-apply (utm-funcs-applicable (second x) (car (third x))))))))
+(defun utm-step (m funcs tape)
+  (remove-n m (append tape
+                      (utm-func-apply
+               		  (utm-funcs-applicable funcs (car tape))))))
 
-;; x is a tag system
-(defun utm-exec (x)
-  (if (< (length (car (cdr (cdr x)))) (car x))
-      (car (cdr (cdr x))) ;; tape smaller than m (or empty)
-      (utm-exec (utm-step x))))
+(defun utm-exec (m funcs tape n)
+  (declare (xargs :measure (nfix n)))
+  (cond
+   ;; both of these are termination conditions
+   ((< (length tape) m) tape)
+   ((zp n) `(nil ,tape))
+   (t (utm-exec m funcs (utm-step m funcs tape) (- n 1)))))
 
-
-
+;; wrapper for utm-exec, which works on a tag system construct
+(defun utm-tag-sys-exec (tag-sys n)
+  (utm-exec (first tag-sys)
+            (second tag-sys)
+            (third tag-sys)
+            n))
